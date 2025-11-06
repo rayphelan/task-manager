fcbcc0b2de9f98ab97858055e0ea545bb7aadfe8
Ray <rayphelan@gmail.com>
2025-11-06 06:54:43 +0400

    chore: add Prettier config repo-wide; setup ESLint for backend; add lint/format scripts

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist/'],
+  rules: {},
+};
+
+
